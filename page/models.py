410cd866235a0109a0e8277e6323815bf34cf432
mojvaf <mojgan@zula-tribe.com>
2025-06-05 10:08:44 +0200

    feat: add image in database

--- page/models.py
+++ page/models.py
@@ -7,6 +7,7 @@ class Authors(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default="fallback.png", blank=True)
 
     def __str__(self):
         return f"Author | {self.first_name} {self.last_name}"
